7b7e6fd4dda02e23efb38d684a6e7292c10e5c65
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2021-12-09 14:23:51 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkUp = galleryItems
+  .map(
+    img =>
+      `<a class="gallery__item" href=${img.original}>
+  <img class="gallery__image" src=${img.preview} alt="${img.description}" />
+  </a>`,
+  )
+  .join('');
+
+galleryEl.innerHTML = galleryItemsMarkUp;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
