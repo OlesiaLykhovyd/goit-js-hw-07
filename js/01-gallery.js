50dcee9ca27f5bc156e498c3968f0ac8f3448261
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2021-12-09 13:19:09 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    img =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href=${img.original}>
+    <img
+      class="gallery__image"
+      src=${img.preview}
+      data-source=${img.original}
+      alt="${img.description}"
+    />
+  </a>
+</div>`,
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryEl.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+<div class="modal">
+<img src="${event.target.dataset.source}" width="1000">
+</div>
+`);
+
+  instance.show();
+}
